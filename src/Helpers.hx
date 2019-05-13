fdb279156fc94a29b6b865e01c54eaa1ce40b368
Franco Lerch <franco.lerch@gmail.com>
2019-05-12 22:15:07 -0300

    f

--- src/Helpers.hx
+++ src/Helpers.hx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 typedef Point = {
     var x:Float;
     var y:Float;
@@ -22,3 +23,30 @@ class Helpers {
         var c2 = a.y - b.y;
         return Math.sqrt(c1*c1 + c2*c2);
     }
+=======
+typedef Point = {
+    var x:Float;
+    var y:Float;
+}
+
+typedef Intersection = {
+    var p:Point;
+    var w:Wall;
+}
+
+class Helpers {
+    public static function toDegrees(x:Float) : Float {
+        return x * (180/Math.PI);
+    }
+
+    public static function toRadians(x:Float) : Float {
+        return x * (Math.PI/180);
+    }
+
+    public static function getDist(a:Point, b:Point) : Float {
+        var c1 = a.x - b.x;
+        var c2 = a.y - b.y;
+        return Math.sqrt(c1*c1 + c2*c2);
+    }
+}
+>>>>>>> Stashed changes
